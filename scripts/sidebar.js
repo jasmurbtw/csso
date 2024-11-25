88e19ab23349c4b3b0f7e9f3fc0e3cd4fa7ad95f
jasmurbtw <103384224+jasmurbtw@users.noreply.github.com>
2024-11-25 14:05:01 +0300

    1

--- scripts/sidebar.js
+++ scripts/sidebar.js
@@ -1,14 +1,41 @@
 const sidebar = document.querySelector('.sidebar');
 const toggleButton = document.querySelector('.sidebar-toggle');
-const ANIMATION_DURATION = 300;
+const content = document.querySelector('.content');
+const header = document.querySelector('header');
+
+function updateHeaderWidth() {
+    const hasVerticalScrollbar = content.scrollHeight > content.clientHeight;
+    if (hasVerticalScrollbar) {
+        header.style.width = `calc(100% - 17px)`;
+    } else {
+        header.style.width = '100%';
+    }
+}
+
+const resizeObserver = new ResizeObserver(() => {
+    updateHeaderWidth();
+});
+resizeObserver.observe(content);
+
+updateHeaderWidth();
+window.addEventListener('resize', updateHeaderWidth);
 
 toggleButton.addEventListener('click', () => {
     sidebar.style.overflowY = 'hidden';
     sidebar.classList.toggle('collapsed');
 
     setTimeout(() => {
-        if (!sidebar.classList.contains('collapsed') && sidebar.scrollHeight > 50) {
-            sidebar.style.overflowY = 'auto';
-        }
-    }, ANIMATION_DURATION);
+        sidebar.style.overflowY = '';
+    });
+
+    setTimeout(updateHeaderWidth);
 });
+
+// const sidebar = document.querySelector('.sidebar');
+// const toggleButton = document.querySelector('.sidebar-toggle');
+// const ANIMATION_DURATION = 300;
+
+// toggleButton.addEventListener('click', () => {
+//     sidebar.style.overflowY = 'hidden';
+//     sidebar.classList.toggle('collapsed');
+// });
