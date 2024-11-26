7a828a807ea17a6be9b7c7659859551fffaa3ff5
jasmurbtw <103384224+jasmurbtw@users.noreply.github.com>
2024-11-26 04:46:48 +0300

    vertical centered menu

--- scripts/sidebar.js
+++ scripts/sidebar.js
@@ -1,46 +1,24 @@
-const sidebar = document.querySelector('.sidebar');
-const toggleButton = document.querySelector('.header-nav-bar');
-const content = document.querySelector('.content');
+const toggleButton = document.querySelector('.header-open-button');
 const header = document.querySelector('header');
-const menuLinks = document.querySelectorAll('.sidebar');
-const ANIMATION_DURATION = 300;
-
-// function updateHeaderWidth() {
-//     const hasVerticalScrollbar = content.scrollHeight > content.clientHeight;
-//     if (hasVerticalScrollbar) {
-//         header.style.width = 'calc(100% - 82px)';
-//     } else {
-//         header.style.width = '100%';
-//     }
-// }
-
-const resizeObserver = new ResizeObserver(() => {
-    // updateHeaderWidth();
-});
-resizeObserver.observe(content);
-
-// updateHeaderWidth();
-// window.addEventListener('resize', updateHeaderWidth);
+const menuLinks = document.querySelectorAll('header a');
 
 toggleButton.addEventListener('click', () => {
-    sidebar.classList.toggle('collapsed');
-
-    // setTimeout(updateHeaderWidth);
+    header.classList.toggle('collapsed');
 });
 
 document.addEventListener('click', (event) => {
-    const isClickInsideSidebar = sidebar.contains(event.target);
-    const isClickOnToggle = event.target.closest('.header-nav-bar');
+    const isClickInsideHeader = header.contains(event.target);
+    const isClickOnToggle = event.target.closest('.header-open-button');
 
-    if (!isClickInsideSidebar && !isClickOnToggle && ! sidebar.classList.contains('collapsed')) {
-        sidebar.classList.add('collapsed');
+    if (!isClickInsideHeader && !isClickOnToggle && ! header.classList.contains('collapsed')) {
+        header.classList.add('collapsed');
     }
 });
 
 menuLinks.forEach(link => {
     link.addEventListener('click', () => {
         setTimeout(() => {
-            sidebar.classList.add('collapsed');
-        }, ANIMATION_DURATION);
+            header.classList.add('collapsed');
+        }, 300);
     });
 });
