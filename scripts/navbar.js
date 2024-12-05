3023d8c7223fd1fd6b6e28c6648b88aaa62275d9
jasmurbtw <103384224+jasmurbtw@users.noreply.github.com>
2024-12-05 03:08:25 +0300

    1

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,20 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
     const headerbutton = document.querySelector('.header-open-button');
     const headermisc = document.querySelector('.header-misc');
-    const headertext = document.querySelectorAll('.header-misc a');
-    const main = document.querySelector('body');
 
     headerbutton.addEventListener('click', () => {
         headermisc.classList.toggle('open');
     });
 
-    headertext.forEach(link => {
+    document.querySelectorAll('.header-misc a').forEach(link => {
         link.addEventListener('click', () => {
             headermisc.classList.remove('open');
         });
     });
 
-    main.addEventListener('click', (event) => {
+    document.querySelector('body').addEventListener('click', (event) => {
         if (!headermisc.contains(event.target) && !headerbutton.contains(event.target)) {
             headermisc.classList.remove('open');
         }
